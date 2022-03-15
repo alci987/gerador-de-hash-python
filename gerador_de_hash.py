fceeae5ab7f724da6fbd318164ec2a97192aebe8
Alciliano da Silva Lima <alcilianodasilva@gmail.com>
2022-03-15 18:18:40 -0300

    Add files via upload

---
+++ gerador_de_hash.py
@@ -0,0 +1,44 @@
+import hashlib
+
+string = input('Digite o texto a ser encriptdo: ')
+
+"""
+se o texto for passado direto no método hashlib.md5, basta adicionr o b
+Ex: hashlib.md5(b"texto")
+
+Se for passado uma variável, deve-se usar a função encode
+Ex: hashlib.md5(string.encode('utf-8'))
+"""
+opcao = 0
+while opcao != 6:
+    print("""######### MENU - Escolha o tipo de hash #########
+                    [1] - MD5
+                    [2] - SHA1
+                    [3] - SHA256
+                    [4] - SHA512
+                    [5] - Exbir todos os hash de uma vez
+                    [6] - Sair""")
+    opcao = int(input('>>>>>> Qual sua opção: '))
+    if opcao == 1:
+        resultado = hashlib.md5(string.encode('utf-8'))
+        print('A Hash MD5 da frase é:', resultado.hexdigest())
+    elif opcao == 2:
+        resultado = hashlib.sha1(string.encode('utf-8'))
+        print('A Hash SHA1 da frase é:', resultado.hexdigest())
+    elif opcao == 3:
+        resultado = hashlib.sha256(string.encode('utf-8'))
+        print('A Hash SHA256 da frase é:', resultado.hexdigest())
+    elif opcao == 4:
+        resultado = hashlib.sha512(string.encode('utf-8'))
+        print('A Hash SHA512 da frase é:', resultado.hexdigest())
+    elif opcao == 5:
+
+        print('MD5: ', hashlib.md5(string.encode('utf-8')).hexdigest())
+        print('SHA1: ', hashlib.sha1(string.encode('utf-8')).hexdigest())
+        print('SHA256: ', hashlib.sha256(string.encode('utf-8')).hexdigest())
+        print('SHA512: ', hashlib.sha512(string.encode('utf-8')).hexdigest())
+    elif opcao == 6:
+        print('Finalizando.....')
+    else:
+        print('Opçao incorreta. Tente novamente!!!')
+    print('=-=' * 10)
